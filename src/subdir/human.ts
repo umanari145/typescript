faee0e6bb59d53166682c91d90646cce5b5412c3
umanari145 <umanari145@gmail.com>
2022-10-29 08:49:01 +0900

    ifの修正

--- src/subdir/human.ts
+++ src/subdir/human.ts
@@ -1,14 +1,25 @@
+// interfaceについて
 interface Human {
     name: string;
     age: number;
     greeting(message: string): void;
 }
 
-
+// オブジェクトに対する継承
 const human: Human = {
     name: 'Yamada',
     age: 38,
-    greeting(message: string): void {
+    greeting(message: string) {
         console.log(`${message}`)
     }
 }
+
+// ifに関しては複数つけることが可能
+class Developers implements Human {
+    constructor(public name: string, public age: number) {
+
+    }
+    greeting(message: string): void {
+        console.log(message);
+    }
+} 
