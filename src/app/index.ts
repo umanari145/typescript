883c16bc7966aca41867810d7d781230f9036480
umanari145 <umanari145@gmail.com>
2023-02-08 22:05:39 +0900

    初期的なデータを記入

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,12 @@
-const sayHello = (name:string) => {
+const konnichiha = (name:string) => {
     return `Hello, ${name}!`;
 }
 
-console.log(sayHello('Kiyohara Kazuhiro'));
+console.log(konnichiha('Kiyohara Kazuhiro'));
 
+
+const calcSample = (a:number, b:number): number => {
+    return a + b;
+}
+
+console.log(calcSample(1,2));
---
+++ src/app/index2.ts
@@ -0,0 +1,5 @@
+const sayHello2 = (name: string) => {
+    return `Hello, ${name}!`
+  }
+  //console.logとほぼ同じ、
+  process.stdout.write(sayHello2('Ochiai Hiromitsu'));
