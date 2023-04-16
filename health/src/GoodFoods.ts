7805651d3c2e79506f94256131e9935284efbdfa
umanari145 <umanari145@gmail.com>
2023-04-16 22:13:53 +0900

    プラス要素を追加

--- health/src/GoodFoods.ts
+++ health/src/GoodFoods.ts
@@ -1,21 +1,29 @@
 export default class GoodFoods {
 
     selector_id: string;
-    element: HTMLElement;
-
 
     constructor(selector_id: string) {
         this.selector_id = selector_id;
-        // !びっくりでnullなし 型アサーション
-        this.element = document.getElementById(this.selector_id)!;
     }
 
     addPoint() {
-        this.element.addEventListener('click', () => {
-            alert('green クリックされた')
+        // !びっくりでnullなし 型アサーション
+        const parent_element:HTMLElement = document.getElementById(this.selector_id)!;
+        parent_element.addEventListener('click', (event: MouseEvent) => {
+            // 型アサーションで強引に決めてしまう
+            const element = event.currentTarget as HTMLElement;
+            console.log('クリックされました');
+            this.addScore(element);
         })
-        this.score 
     }
 
+    addScore(parent_element:HTMLElement) {
+        const score_element: HTMLElement = parent_element.querySelector('.each_food_score')!;
+        let score: string = score_element.innerHTML;
+        let score_num: number = parseInt(score);
+        score_num++;
+        console.log('今点数はこれだけです。'+ score_num);
+        score_element.innerHTML = String(score_num)
+    }
 
 }
--- health/src/health.ts
+++ health/src/health.ts
@@ -3,5 +3,5 @@ import GoodFoods from "./GoodFoods";
 const green: GoodFoods = new GoodFoods('green');
 green.addPoint();
 
-const orange:GoodFoods = new GoodFoods('ogrange');
+const orange:GoodFoods = new GoodFoods('orange');
 orange.addPoint();
