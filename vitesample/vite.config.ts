8b062dc27870ce4e9e8dd29301f0bdb3baa0d532
umanari145 <umanari145@gmail.com>
2025-09-28 14:11:24 +0900

    vite追加

---
+++ vitesample/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    host: true, // true に設定すると、LAN やパブリックアドレスを含むすべてのアドレスをリッスン
+    port: 5173, // 開発サーバーが使用するポート番号
+  },
+})
