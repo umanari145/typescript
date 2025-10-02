5889d5bf5997549a590ece609d1d77ce00dfcc0b
umanari145 <umanari145@gmail.com>
2025-10-02 23:15:35 +0900

    viteの設定ファイルをルートディレクトリに変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: 'vitesample',
+  base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: '../dist',
+  },
   server: {
     host: true, // true に設定すると、LAN やパブリックアドレスを含むすべてのアドレスをリッスン
     port: 5173, // 開発サーバーが使用するポート番号
